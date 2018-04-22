6a8b041ca08ede6ac1f6888a17734fbf510a9cf3
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-22 16:42:11 +0100

    Add files via upload

---
+++ js/index_original.js
@@ -0,0 +1,47 @@
+var mac="B8:27:EB:D2:4A:3F"; /* MAC address of Raspberry Pi Bluetooth module */
+
+$(document).on('pageinit', function(){
+  check_conn();
+})
+
+// connect to the raspberry pi
+function connect(){
+  bluetoothSerial.connect(mac, con_success, con_failure);
+}
+
+// connection successful
+function con_success(){
+  $("#display").html("Connected\n")
+  bluetoothSerial.read(function (data){
+    $("#display").html(data+"\n");
+  });
+  disconnect();
+}
+
+// connection unsuccessful
+function con_failure(){
+  alert("ERROR CONNECTING");
+}
+
+// disconnect bluetooth
+function disconnect(){
+  bluetoothSerial.disconnect(dis_success, dis_failure);
+}
+
+// disconnection successful
+function dis_success(){
+  alert("Disconnected");
+}
+
+// disconnection unsuccessful
+function dis_failure(){
+  alert("ERROR DISCONNECTING");
+}
+
+function check_conn(){
+  bluetoothSerial.isEnabled(connect, bt_off);
+}
+
+function bt_off(){
+  alert("Bluetooth not on");
+}
