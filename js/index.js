8ff6b90289fac8b2e0e4bfa4dc5f525a9fecd7b3
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-22 11:50:33 +0100

    First try
    Contains only connect and read

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var mac="B8:27:EB:D2:4A:3F"; /* MAC address of Raspberry Pi Bluetooth module */
+
+
+// connect to the raspberry pi
+function connect(){
+  bluesy.connect(mac, con_success, con_failure);
+}
+
+// connection successful
+function con_success(){
+  bluesy.read(function (data){
+    $("#display").text(data+"\n");
+  });
+}
+
+// connection unsuccessful
+function con_failure(){
+  alert("ERROR CONNECTING");
+}
+
+// disconnect bluetooth
+function disconnect(){
+  bluesy.disconnect(dis_success, dis_failure);
+}
+
+// disconnection successful
+function dis_success(){
+  alert("Disconnected");
+}
+
+// disconnection unsuccessful
+function dis_failure(){
+  alert("ERROR DISCONNECTING");
+}
+
+connect();
