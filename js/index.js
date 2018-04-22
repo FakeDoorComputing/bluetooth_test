3adcf4ae3c16a5e3e553d9639bb65532a61093b4
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-22 22:57:16 +0100

    Added while true to function to create a loop
    Keeps the Bluetooth listening

--- js/index.js
+++ js/index.js
@@ -1,54 +1,60 @@
 var mac="B8:27:EB:D2:4A:3F"; /* MAC address of Raspberry Pi Bluetooth module */
 
 document.addEventListener('deviceready',
-  function(){check_conn()},
-  false);
+  function(){
+    check_conn()
+  },false);
 
-// connect to the raspberry pi
-function connect(){
-  console.log("bluetooth is on");
-  bluetoothSerial.connect(mac, con_success, con_failure);
-}
+  function check_conn(){
+    bluetoothSerial.isEnabled(connect, bt_off);
+  }
 
-// connection successful
-function con_success(){
-  $("#display").html("Connected\n")
-  bluetoothSerial.write("connected");
-  bluetoothSerial.subscribe("\n",function (data){
-    $("#display").html("*"+data+"*");
-    if(data=="locked\n"){
-      $("#display").html("Locked");
-    }
-    else{
-      $("#display").html("Unlocked");
-    }
-  });
-}
 
-// connection unsuccessful
-function con_failure(){
-  alert("ERROR CONNECTING");
-}
+  // connect to the raspberry pi
+  function connect(){
+    console.log("bluetooth is on");
+    bluetoothSerial.connect(mac, function(){
+      while true{
+        con_success();
+      }
+    }, con_failure);
+  }
 
-// disconnect bluetooth
-function disconnect(){
-  bluetoothSerial.disconnect(dis_success, dis_failure);
-}
+  // connection successful
+  function con_success(){
+    bluetoothSerial.write("connected");
+    bluetoothSerial.subscribe("\n",function (data){
+      $("#display").html("*"+data+"*");
+      if(data=="locked\n"){
+        $("#display").html("Locked");
+      }
+      else{
+        $("#display").html("Unlocked");
+      }
+    });
+    bluetoothSerial.clear();
+  }
 
-// disconnection successful
-function dis_success(){
-  alert("Disconnected");
-}
+  // connection unsuccessful
+  function con_failure(){
+    alert("ERROR CONNECTING");
+  }
 
-// disconnection unsuccessful
-function dis_failure(){
-  alert("ERROR DISCONNECTING");
-}
+  // disconnect bluetooth
+  function disconnect(){
+    bluetoothSerial.disconnect(dis_success, dis_failure);
+  }
 
-function check_conn(){
-  bluetoothSerial.isEnabled(connect, bt_off);
-}
+  // disconnection successful
+  function dis_success(){
+    alert("Disconnected");
+  }
 
-function bt_off(){
-  alert("Bluetooth not on");
-}
+  // disconnection unsuccessful
+  function dis_failure(){
+    alert("ERROR DISCONNECTING");
+  }
+
+  function bt_off(){
+    alert("Bluetooth not on");
+  }
