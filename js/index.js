35f329dc3b420e8ea18ef069ee9d60f451b188bb
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-22 16:57:01 +0100

    changed the js files

--- js/index.js
+++ js/index.js
@@ -1,152 +1,47 @@
-/*
-    SimpleSerial index.js
-    Created 7 May 2013
-    Modified 9 May 2013
-    by Tom Igoe
-*/
+var mac="B8:27:EB:D2:4A:3F"; /* MAC address of Raspberry Pi Bluetooth module */
 
+document.addEventListener('deviceready',
+  function(){check_conn()},
+  false);
 
-var app = {
-    macAddress: "AA:BB:CC:DD:EE:FF",  // get your mac address from bluetoothSerial.list
-    chars: "",
+// connect to the raspberry pi
+function connect(){
+  bluetoothSerial.connect(mac, con_success, con_failure);
+}
 
-/*
-    Application constructor
- */
-    initialize: function() {
-        this.bindEvents();
-        console.log("Starting SimpleSerial app");
-    },
-/*
-    bind any events that are required on startup to listeners:
-*/
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        connectButton.addEventListener('touchend', app.manageConnection, false);
-    },
+// connection successful
+function con_success(){
+  $("#display").html("Connected\n")
+  bluetoothSerial.read(function (data){
+    $("#display").html(data+"\n");
+  });
+  disconnect();
+}
 
-/*
-    this runs when the device is ready for user interaction:
-*/
-    onDeviceReady: function() {
-        // check to see if Bluetooth is turned on.
-        // this function is called only
-        //if isEnabled(), below, returns success:
-        var listPorts = function() {
-            // list the available BT ports:
-            bluetoothSerial.list(
-                function(results) {
-                    app.display(JSON.stringify(results));
-                },
-                function(error) {
-                    app.display(JSON.stringify(error));
-                }
-            );
-        }
+// connection unsuccessful
+function con_failure(){
+  alert("ERROR CONNECTING");
+}
 
-        // if isEnabled returns failure, this function is called:
-        var notEnabled = function() {
-            app.display("Bluetooth is not enabled.")
-        }
+// disconnect bluetooth
+function disconnect(){
+  bluetoothSerial.disconnect(dis_success, dis_failure);
+}
 
-         // check if Bluetooth is on:
-        bluetoothSerial.isEnabled(
-            listPorts,
-            notEnabled
-        );
-    },
-/*
-    Connects if not connected, and disconnects if connected:
-*/
-    manageConnection: function() {
+// disconnection successful
+function dis_success(){
+  alert("Disconnected");
+}
 
-        // connect() will get called only if isConnected() (below)
-        // returns failure. In other words, if not connected, then connect:
-        var connect = function () {
-            // if not connected, do this:
-            // clear the screen and display an attempt to connect
-            app.clear();
-            app.display("Attempting to connect. " +
-                "Make sure the serial port is open on the target device.");
-            // attempt to connect:
-            bluetoothSerial.connect(
-                app.macAddress,  // device to connect to
-                app.openPort,    // start listening if you succeed
-                app.showError    // show the error if you fail
-            );
-        };
+// disconnection unsuccessful
+function dis_failure(){
+  alert("ERROR DISCONNECTING");
+}
 
-        // disconnect() will get called only if isConnected() (below)
-        // returns success  In other words, if  connected, then disconnect:
-        var disconnect = function () {
-            app.display("attempting to disconnect");
-            // if connected, do this:
-            bluetoothSerial.disconnect(
-                app.closePort,     // stop listening to the port
-                app.showError      // show the error if you fail
-            );
-        };
+function check_conn(){
+  bluetoothSerial.isEnabled(connect, bt_off);
+}
 
-        // here's the real action of the manageConnection function:
-        bluetoothSerial.isConnected(disconnect, connect);
-    },
-/*
-    subscribes to a Bluetooth serial listener for newline
-    and changes the button:
-*/
-    openPort: function() {
-        // if you get a good Bluetooth serial connection:
-        app.display("Connected to: " + app.macAddress);
-        // change the button's name:
-        connectButton.innerHTML = "Disconnect";
-        // set up a listener to listen for newlines
-        // and display any new data that's come in since
-        // the last newline:
-        bluetoothSerial.subscribe('\n', function (data) {
-            app.clear();
-            app.display(data);
-        });
-    },
-
-/*
-    unsubscribes from any Bluetooth serial listener and changes the button:
-*/
-    closePort: function() {
-        // if you get a good Bluetooth serial connection:
-        app.display("Disconnected from: " + app.macAddress);
-        // change the button's name:
-        connectButton.innerHTML = "Connect";
-        // unsubscribe from listening:
-        bluetoothSerial.unsubscribe(
-                function (data) {
-                    app.display(data);
-                },
-                app.showError
-        );
-    },
-/*
-    appends @error to the message div:
-*/
-    showError: function(error) {
-        app.display(error);
-    },
-
-/*
-    appends @message to the message div:
-*/
-    display: function(message) {
-        var display = document.getElementById("message"), // the message div
-            lineBreak = document.createElement("br"),     // a line break
-            label = document.createTextNode(message);     // create the label
-
-        display.appendChild(lineBreak);          // add a line break
-        display.appendChild(label);              // add the message node
-    },
-/*
-    clears the message div:
-*/
-    clear: function() {
-        var display = document.getElementById("message");
-        display.innerHTML = "";
-    }
-};      // end of app
+function bt_off(){
+  alert("Bluetooth not on");
+}
