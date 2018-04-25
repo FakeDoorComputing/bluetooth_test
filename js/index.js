55f02229febb52f8594551977d45f97865068a22
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-25 11:12:34 +0100

    changed where the loop starts

--- js/index.js
+++ js/index.js
@@ -1,38 +1,48 @@
 var mac="B8:27:EB:D2:4A:3F"; /* MAC address of Raspberry Pi Bluetooth module */
 
-document.addEventListener('deviceready',
-  function(){
-    check_conn()
+// (document).ready for desktop, 'deviceready' for mobile
+$(document).ready(function(){
+/*document.addEventListener('deviceready',
+  function(){ */
+    $("button").click(function(){
+      /* when the button is clicked, check if bluetooth is on */
+      check_conn()
+    })
   },false);
 
   function check_conn(){
+    /* if bluetooth is on, connect, else show warning */
     bluetoothSerial.isEnabled(connect, bt_off);
   }
 
-
   // connect to the raspberry pi
   function connect(){
-    console.log("bluetooth is on");
+    console.log("bluetooth is on"); /* for development */
+    /* connect to the hard coded MAC address of the PI */
     bluetoothSerial.connect(mac, function(){
-      while(true){
-        con_success();
-      }
+      /* send connected signal to pi */
+      bluetoothSerial.write("connected");
+      con_success();
     }, con_failure);
   }
 
   // connection successful
   function con_success(){
-    bluetoothSerial.write("connected");
-    bluetoothSerial.subscribe("\n",function (data){
-      $("#display").html("*"+data+"*");
-/*      if(data=="locked\n"){
-        $("#display").html("Locked");
-      }
-      else{
-        $("#display").html("Unlocked");
-      }*/
-    });
-    bluetoothSerial.clear();
+    /* keep looping through con_success*/
+    while(true){
+      /* keep listening for data from pi */
+      bluetoothSerial.subscribe("\n",function (data){
+        $("#display").html("*"+data+"*");
+  /*      if(data=="locked\n"){
+          $("#display").html("Locked");
+        }
+        else{
+          $("#display").html("Unlocked");
+        }   */
+      });
+      /* empty the buffer */
+      bluetoothSerial.clear();
+    }
   }
 
   // connection unsuccessful
@@ -42,6 +52,7 @@ document.addEventListener('deviceready',
 
   // disconnect bluetooth
   function disconnect(){
+    /* disconnect from the bluetooth */
     bluetoothSerial.disconnect(dis_success, dis_failure);
   }
 
